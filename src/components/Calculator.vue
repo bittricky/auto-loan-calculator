20bc34fe0fda85bc1ebba4af008f1c46d1da785b
Mitul Patel <me@mitulpa.tel>
2025-03-24 11:45:50 -0400

    fix(component): updated color scheme to red

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -591,7 +591,7 @@ const formatCurrency = (value: number) => {
             <br />
             <button 
               @click="exportToCSV" 
-              class="w-full bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-3 px-4 rounded-lg flex items-center justify-center mb-6"
+              class="w-full bg-red-500 hover:bg-red-900 transition-colors text-white font-bold py-3 px-4 rounded-lg flex items-center justify-center mb-6"
             >
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
